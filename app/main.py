2a49400ac18d7c1fc3e7d4cb35a39dbd7c0d33c1
RAVIKANTH K S <76530433+ravikanth-io@users.noreply.github.com>
2026-04-03 15:37:50 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,9 +3,15 @@ from app.db.database import engine, Base
 from app.api.routes import router
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from fastapi import FastAPI
+from app.routes.notes import router as notes_router
+from app.auth.routes import router as auth_router
 
-port = int(os.environ.get("PORT", 10000))
 app = FastAPI()
+
+app.include_router(notes_router)
+app.include_router(auth_router)
+port = int(os.environ.get("PORT", 10000))
 
 app.add_middleware(
     CORSMiddleware,
