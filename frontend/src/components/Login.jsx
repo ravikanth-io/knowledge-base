f9a43d2a4fa5ef49e077537d722c8ff56e7e16ea
RAVIKANTH K S <76530433+ravikanth-io@users.noreply.github.com>
2026-04-03 15:59:17 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,1 +1,21 @@
+import axios from "axios";
 
+const API = "https://knowledge-base-api-ly0h.onrender.com";
+
+export default function Login({ setToken }) {
+  const login = async () => {
+    const res = await axios.post(`${API}/login`, {
+      username: "admin",
+      password: "1234",
+    });
+
+    setToken(res.data.access_token);
+    alert("Logged in");
+  };
+
+  return (
+    <button onClick={login} className="bg-blue-500 p-2 mb-4">
+      Login
+    </button>
+  );
+}
